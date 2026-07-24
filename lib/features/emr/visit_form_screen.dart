d962d52f970cccacc2eb213563c0105f15e9de85
brunodoss <brunodoss.j@contus.in>
2026-07-24 08:34:22 +0530

    UI changes

--- lib/features/emr/visit_form_screen.dart
+++ lib/features/emr/visit_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:maran/core/messaging/app_messenger.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
